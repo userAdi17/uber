dd5313b3f0aa1ecfcade0b79b95acb633642961b
userAdi17 <jamangulovaa0@gmail.com>
2020-12-16 18:45:28 +0600

    initial

---
+++ script/script.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", () =>{
+    const menu = document.querySelector('.menu')
+    menuItem =  document.querySelector('.menu_item')
+    burger = document.querySelector('.burger')
+    burger.addEventListener('click', () =>{
+        burger.classList.toggle('burger_active')
+        menu.classList.toggle('menu_active')
+    })
+    menuItem.forEach(item => {
+        item.addEventListener('click', () =>{
+            burger.classList.toggle('burger_active')
+            menu.classList.toggle('menu_active')
+        })
+    })
+})
